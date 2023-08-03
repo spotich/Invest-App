ec7bd4d002d7ee0cf7317b6c409a16d141ad6234
spotich <popenko1337@gmail.com>
2023-08-03 20:20:23 +0700

    US-3 - models/UserModel.php - create function addUser

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -5,12 +5,17 @@ use application\core\Model;
 
 class UserModel extends Model
 {
-
-    public function getUserByEmail($email): array {
+    public function getUserByEmail($email) {
         $params = ['email' => $email];
         $result = $this->db->row('SELECT * FROM users WHERE email = :email', $params);
-        return $result[0];
+        return (!$result) ? null : $result[0];
     }
 
-
+    public function addUser($user = []): bool {
+        if (!empty($user)) {
+            $user['tfa'] = bin2hex(random_bytes(20));
+            $result = $this->db->executeQuery('INSERT INTO users (name, surname, email, role, password, tfa) VALUES (:name, :surname, :email, :role, :password, :tfa)', $user);
+        }
+        return $result;
+    }
 }
