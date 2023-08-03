2aa3b0e301e000dc5aec340d2f1e5d73536a58b0
spotich <popenko1337@gmail.com>
2023-08-03 20:04:41 +0700

    US-3 - add email authentication (failed)

--- application/controllers/AuthenticationController.php
+++ application/controllers/AuthenticationController.php
@@ -5,6 +5,7 @@ namespace application\controllers;
 use application\core\View;
 use application\databases\DatabaseMySQL;
 use application\models\UserModel;
+use JetBrains\PhpStorm\NoReturn;
 
 class AuthenticationController
 {
@@ -16,8 +17,9 @@ class AuthenticationController
         $this->model = new UserModel($database);
     }
 
-    public static function  getCurrentUserData(): array {
-        if (session_status() === PHP_SESSION_ACTIVE and isset($_SESSION['auth'])) {
+    public static function getCurrentUserData(): array|null
+    {
+        if (session_status() === PHP_SESSION_ACTIVE and isset($_SESSION['authenticated'])) {
             $userData = [
                 'name' => $_SESSION['name'],
                 'surname' => $_SESSION['surname'],
@@ -25,45 +27,65 @@ class AuthenticationController
                 'role' => $_SESSION['role'],
             ];
         } else {
-            $userData = [];
+            $userData = null;
         }
         return $userData;
     }
 
     public function authenticateUser(): void
     {
-        if (empty($_POST)) {
-            if (session_status() === PHP_SESSION_ACTIVE) {
+        if (session_status() === PHP_SESSION_ACTIVE and isset($_SESSION['authenticated'])) {
+            View::redirect('/profile');
+        } elseif (isset($_POST['email']) and isset($_POST['password'])) {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $user = $this->model->getUserByEmail($email);
+            if (is_null($user)) {
+                echo 'Unknown user';
+                exit;
+            } elseif (md5($password) == $user['password']) {
+                $this->twoFactor($user);
+                $_SESSION['authenticated'] = true;
+                $_SESSION['name'] = $user['name'];
+                $_SESSION['surname'] = $user['surname'];
+                $_SESSION['email'] = $user['email'];
+                $_SESSION['role'] = $user['role'];
                 View::redirect('/profile');
-            }
-        } else {
-            if (!empty($_POST)) {
-                $email = empty($_POST['email']) ? null : $_POST['email'];
-                $password = empty($_POST['password']) ? null : $_POST['password'];
-                $user = $this->model->getUserByEmail($email);
-
-                if (md5($password) == $user['password']) {
-                    $_SESSION['authenticated'] = true;
-                    $_SESSION['name'] = $user['name'];
-                    $_SESSION['surname'] = $user['surname'];
-                    $_SESSION['email'] = $user['email'];
-                    $_SESSION['role'] = $user['role'];
-                    View::redirect('/profile');
-                }
+            } else {
+                echo 'Wrong password';
+                exit;
             }
         }
     }
 
-    public function logoutUser(): void {
-        if (session_status() === PHP_SESSION_ACTIVE) {
+    #[NoReturn] public function logoutUser(): void
+    {
+        if (session_status() === PHP_SESSION_ACTIVE and isset($_SESSION['authenticated'])) {
             session_unset();
             session_destroy();
         }
         View::redirect('/');
     }
 
+    private function twoFactor($user) {
+        if(isset($user['tfa'])) {
+            extract($user);
+            $to = $email;
+            $subject = 'User verification';
+            ob_start();
+            require dirname(__DIR__, 1) . "/views/layouts/letter.php";
+            $message = ob_get_clean();
+            $headers[] = 'MIME-Version: 1.0';
+            $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+            $result = mail($to, $subject, $message, implode("\r\n", $headers));
+        } else {
+            return null;
+        }
+    }
+
     public function registerUser(): void
     {
-        // ..
+        //..
     }
+
 }
---
+++ application/views/layouts/letter.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <title>Two Factor Authentication</title>
+</head>
+<body>
+  <p>Someone was trying to enter your account</p><br>
+  <p>if it was you, please follow the link</p>
+  <a href=\"www.google.com\">Veify</a>
+</body>
+</html>
