dab490172377de041df490ad8577902133b45051
spotich <popenko1337@gmail.com>
2023-08-15 14:35:13 +0700

    US2 - add payment popup

--- application/views/templates/detailed.php
+++ application/views/templates/detailed.php
@@ -4,30 +4,33 @@
         <div class="col-1"></div>
         <div class="col-10 project">
             <h2 class="mb-5 mt-3"><?php echo $project['description_short']; ?></h2>
-            <img src="/img/projects/<?php echo $project['cover']; ?>" class="img-fluid w-100 border border-2 rounded-3 mb-5">
+            <img src="/img/projects/<?php echo $project['cover']; ?>"
+                 class="img-fluid w-100 border border-2 rounded-3 mb-5">
             <h3 class="mb-5"><?php echo $project['description_long']; ?></h3>
             <div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
                 <?php foreach ($project['team_members'] as $team_member): ?>
-                <div class="col d-flex">
-                    <div class="border border-2 rounded-3 p-4 mb-4">
-                        <img src="/img/users/<?php echo $team_member['avatar']; ?>" class="img-fluid rounded-3 mb-3">
-                        <h4><b><?php echo $team_member['name']; ?> <?php echo $team_member['surname']; ?></b></h4>
-                        <h5><?php echo $team_member['role']; ?></h5>
-                        <p><?php echo $team_member['description']; ?></p>
+                    <div class="col d-flex">
+                        <div class="border border-2 rounded-3 p-4 mb-4">
+                            <img src="/img/users/<?php echo $team_member['avatar']; ?>"
+                                 class="img-fluid rounded-3 mb-3">
+                            <h4><b><?php echo $team_member['name']; ?><?php echo $team_member['surname']; ?></b></h4>
+                            <h5><?php echo $team_member['role']; ?></h5>
+                            <p><?php echo $team_member['description']; ?></p>
+                        </div>
                     </div>
-                </div>
                 <?php endforeach; ?>
             </div>
             <h5 class="mt-5">We have collected <?php echo $project['progressbar']; ?>%</h5>
             <div class="row">
                 <div class="col-10">
-                    <div class="progress" role="progressbar" aria-label="Animated striped example"  aria-valuemin="0" aria-valuemax="100">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?php echo $project['progressbar']; ?>%"></div>
+                    <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuemin="0"
+                         aria-valuemax="100">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated"
+                             style="width: <?php echo $project['progressbar']; ?>%"></div>
                     </div>
                 </div>
                 <div class="col-2">
-                    <button type="button" class="btn btn-lg btn-primary w-100" onclick="location.href='/login'">
-                        <img class="icon" src="../img/login.png">
+                    <button id="payment-button" type="button" class="btn btn-lg btn-primary w-100">
                         Invest
                     </button>
                 </div>
@@ -39,8 +42,49 @@
                 </div>
                 <div class="col-2"></div>
             </div>
-
+            <div id="payment" class="modal">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-2"></div>
+                        <div class="modal-content col-10">
+                            <h2 class="popup-title"><?php echo $project['name']; ?></h2>
+                            <p class="text-center">To commit payment fill the form down below</p>
+                            <div class="container text-center">
+                                <div class="row">
+                                    <div class="col"></div>
+                                    <div class="col">
+                                        <form method="post" action="">
+                                            <div class="form-group mb-5">
+                                                <div class="row">
+                                                    <input id="email" type="text"
+                                                           class="form-control form-control-lg mb-2"
+                                                           placeholder="Credit Card Number" maxlength="16">
+                                                </div>
+                                                <div class="row">
+                                                    <input id="expiration-date" type="text" placeholder="03/23"
+                                                           class="form-control form-control-lg col me-3" maxlength="4">
+                                                    <input id="code-cvv" type="password"
+                                                           class="form-control form-control-lg col"
+                                                           placeholder="CVV" pattern="^\d{3}$" maxlength="3">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <button type="submit" class="btn btn-primary btn-lg mb-5">Invest
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="col"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-2"></div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="col-1"></div>
     </div>
 </div>
+
+<script type="text/javascript" src="../js/popup.js"></script>
--- public/js/popup.js
+++ public/js/popup.js
@@ -1,23 +1,12 @@
-var signInModal = document.getElementById("sign-in-modal");
-var signUpModal = document.getElementById("sign-up-modal");
-var signInButton = document.getElementById("sign-in-button");
-var signUpButton = document.getElementById("sign-up-button");
+let paymentModal = document.getElementById("payment");
+let paymentButton = document.getElementById("payment-button");
 
-function showModal (modalID) {
-    modalID.style.display = "block";
-}
-
-signInButton.onclick = function () {
-    signInModal.style.display = "block";
-}
-
-signUpButton.onclick = function () {
-    signUpModal.style.display = "block";
+paymentButton.onclick = function () {
+    paymentModal.style.display = "block";
 }
 
 window.onclick = function(event) {
-    if (event.target == signUpModal || event.target == signInModal) {
-        signUpModal.style.display = "none";
-        signInModal.style.display = "none";
+    if (event.target === paymentModal) {
+        paymentModal.style.display = "none";
     }
 }
