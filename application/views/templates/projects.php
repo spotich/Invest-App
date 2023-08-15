c63e16ca17271f346f982638369af572ef90d1df
spotich <popenko1337@gmail.com>
2023-08-15 16:54:35 +0700

    add responsiveness to projects page

--- application/views/templates/projects.php
+++ application/views/templates/projects.php
@@ -4,10 +4,10 @@
         <?php foreach($projects as $project): ?>
             <a href="projects/<?php echo $project['id']; ?>" draggable="false">
                 <div class="container row border border-2 rounded-3 mb-4 px-5 py-4">
-                    <div class="col-4 container ">
+                    <div class="col-xl-6 col-xxl container">
                         <img src="img/projects/<?php echo $project['cover']; ?>" class="img-fluid w-100">
                     </div>
-                    <div class="col-4 align-self-center">
+                    <div class="col-xl-6 col-xxl align-self-center">
                         <div class="row mb-3">
                             <h2><?php echo $project['name']; ?></h2>
                         </div>
@@ -15,8 +15,8 @@
                             <h3><?php echo $project['description_short']; ?></h3>
                         </div>
                     </div>
-                    <div class="col"></div>
-                    <div class="col-3 align-self-center">
+
+                    <div class="col-xl col-xxl-3 align-self-center">
                         <div class="row mb-3">
                             <h2>Category</h2>
                         </div>
