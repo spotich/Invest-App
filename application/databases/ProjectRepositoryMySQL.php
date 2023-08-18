3458fef3f85165153ff80fadb793425ce320bc23
spotich <popenko1337@gmail.com>
2023-08-18 20:06:56 +0700

    US2 - ProjectRepositoryMySQL - add project tags

--- application/databases/ProjectRepositoryMySQL.php
+++ application/databases/ProjectRepositoryMySQL.php
@@ -3,20 +3,19 @@
 namespace InvestApp\application\databases;
 
 use InvestApp\application\contracts\ProjectRepository;
-class ProjectRepositoryMySQL extends DatabaseMySQL implements ProjectRepository
+class ProjectRepositoryMySQL extends RepositoryMySQL implements ProjectRepository
 {
     public function getAllProjects(): ?array
     {
-        $result = $this->getRow('SELECT p.*, pc.name as cover FROM projects p JOIN project_covers pc ON p.id = pc.project_id ORDER BY p.created_at DESC');
+        $result = $this->getRow('SELECT p.*, group_concat(t.name) as tags FROM projects p JOIN projects_tags pt ON p.id = pt.project_id JOIN tags t ON pt.tag_id = t.id GROUP BY p.id');
         return is_array($result) ? $result : null;
     }
-
 
     public function getProjectById(int $id): ?array
     {
         $params = ['id' => $id];
-        $result = $this->getRow('SELECT p.*, pc.name as cover FROM projects p JOIN project_covers pc ON p.id = pc.project_id WHERE p.id = :id', $params);
-        if (is_array($result)) {
+        $result = $this->getRow('SELECT p.*, group_concat(t.name) as tags FROM projects p JOIN projects_tags pt ON p.id = pt.project_id JOIN tags t ON pt.tag_id = t.id  WHERE p.id = :id GROUP BY p.id', $params);
+        if (isset($result[0])) {
             $result[0]['team_members'] = $this->getRow('SELECT u.name, u.surname, ua.name as avatar, tm.role, tm.description FROM users u JOIN team_members tm ON u.id = tm.user_id JOIN user_avatars ua ON u.id = ua.user_id WHERE tm.project_id = :id', $params);
         } else {
              return null;
