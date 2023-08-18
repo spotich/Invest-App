40f5ec8e90c5152300ee33be8adf3244ad2bc605
spotich <popenko1337@gmail.com>
2023-08-18 19:54:54 +0700

    US2 - Repository.php - rename class

---
+++ application/contracts/Repository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace InvestApp\application\contracts;
+
+interface Repository
+{
+    public function executeQuery($query, $params = []);
+    public function getRow($query, $params = []);
+    public function getColumn($sql, $params = []);
+}
