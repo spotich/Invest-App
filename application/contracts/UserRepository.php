b9ca58dd95644a25c14efef3f19fb2ebfaa80dfb
spotich <popenko1337@gmail.com>
2023-08-18 20:02:22 +0700

    US2 - UserRepository.php - make method names more clear

--- application/contracts/UserRepository.php
+++ application/contracts/UserRepository.php
@@ -2,16 +2,16 @@
 
 namespace InvestApp\application\contracts;
 
-interface UserRepository extends Database
+interface UserRepository extends Repository
 {
     public function getUserByEmail(string $email): ?array;
     public function getUserById(int $id): ?array;
     public function getUserAvatar(int $id): ?string;
     public function createNewUser(array $user): ?int;
     public function updateUser(array $user): ?bool;
-    public function newAuthenticationCodeForUser(int $id): ?string;
-    public function newExpirationTimeForUser(int $id);
-    public function newResetCodeForUser(int $id): ?string;
+    public function setVerificationCodeForUser(int $id, string $verificationCode): void;
+    public function setExpirationTimeForUser(int $id, string $expirationTime): void;
+    public function setRecoveryCodeForUser(int $id, string $recoveryCode, string $expirationTime): void;
     public function getResetDataForUser(int $id): ?array;
     public function getAuthenticationDataForUser(int $id): ?array;
 }
