5d1f5572a9398c78521c3f1341617cf1ebc47d9e
spotich <popenko1337@gmail.com>
2023-08-18 19:51:24 +0700

    US2 - SessionController.php - store user as object instead of array

--- application/controllers/SessionController.php
+++ application/controllers/SessionController.php
@@ -3,51 +3,26 @@
 namespace InvestApp\application\controllers;
 
 use InvestApp\application\core\View;
-use JetBrains\PhpStorm\NoReturn;
+use InvestApp\application\models\User;
 
 class SessionController
 {
-    public static function getCurrentUserData(): array|null
+    private View $view;
+
+    public static function getCurrentUser(): ?User
     {
-        if (self::isCurrentUserActive()) {
-            $userData = [
-                'name' => $_SESSION['name'],
-                'surname' => $_SESSION['surname'],
-                'email' => $_SESSION['email'],
-                'role' => $_SESSION['role'],
-                'avatar' => $_SESSION['avatar'],
-            ];
-        } else {
-            $userData = null;
-        }
-        return $userData;
+        return (isset($_SESSION['user']) and ($_SESSION['user'] instanceof User)) ? $_SESSION['user'] : null;
     }
 
-    public static function setCurrentUserData(array $currentUser): bool
+    public static function setCurrentUser(User $currentUser): void
     {
-        $ok = (isset($currentUser['name']) and isset($currentUser['surname']) and isset($currentUser['email']) and isset($currentUser['password']) and isset($currentUser['avatar']));
-        if ($ok) {
-            $_SESSION['authenticated'] = true;
-            $_SESSION['name'] = $currentUser['name'];
-            $_SESSION['surname'] = $currentUser['surname'];
-            $_SESSION['email'] = $currentUser['email'];
-            $_SESSION['role'] = $currentUser['role'];
-            $_SESSION['avatar'] = $currentUser['avatar'];
-        }
-        return $ok;
+        $_SESSION['user'] = $currentUser;
     }
 
-    #[NoReturn] public static function clearCurrentUserData():void
+    public function forgetCurrenUser(): void
     {
-        if (self::isCurrentUserActive()) {
-            session_unset();
-            session_destroy();
-        }
-        View::redirect('/');
-    }
-
-    public static function isCurrentUserActive(): bool
-    {
-        return (session_status() === PHP_SESSION_ACTIVE and isset($_SESSION['authenticated']) and $_SESSION['authenticated'] === true and isset($_SESSION['name']) and isset($_SESSION['surname']) and isset($_SESSION['email']) and isset($_SESSION['role']) and isset($_SESSION['avatar']));
+        unset($_SESSION['user']);
+        $this->view = new View();
+        $this->view->redirectToUrl('/');
     }
 }
