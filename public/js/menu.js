49edd4db09c782e9e2ab89c6afe947c2059af9f7
spotich <popenko1337@gmail.com>
2023-08-21 15:49:03 +0700

    US2 - js/menu.js, css/style.css - resolve class collision

--- public/js/menu.js
+++ public/js/menu.js
@@ -6,6 +6,6 @@ const activePage = window.location.pathname;
 for (let i = 0; i < navItems.length; i++) {
     let link = navItems[i].getElementsByClassName("nav-link")[0].getAttribute("href");
     if (link === activePage ) {
-        navItems[i].classList.add('active');
+        navItems[i].classList.add('active-item');
     }
 }
