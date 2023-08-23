2fefa667aec08d62cc4a0566641b4f3f69cc874f
spotich <popenko1337@gmail.com>
2023-08-23 13:59:00 +0700

    US-2-3 - config/routes.php - add routes for admin, change delmitters for regular expressions

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,54 +7,75 @@ use InvestApp\application\controllers\SessionController;
 use InvestApp\application\controllers\RecoveryController;
 use InvestApp\application\controllers\ProfileController;
 use InvestApp\application\controllers\ProjectController;
+use InvestApp\application\controllers\AdminController;
 
 return [
-    '/^$/' => [
+    '#^$#' => [
         'controller' => HomeController::class,
         'action' => 'index',
     ],
 
-    '/^projects$/' => [
+    '#^projects$#' => [
         'controller' => ProjectController::class,
-        'action' => 'showProjects',
+        'action' => 'showAllProjects',
     ],
 
-    '/^projects\/(\d+)$/' => [
+    '#^projects\/(\d+)$#' => [
         'controller' => ProjectController::class,
         'action' => 'showDetailedProject',
     ],
 
-    '/^profile$/' => [
+    '#^create$#' => [
+        'controller' => ProjectController::class,
+        'action' => 'createProject',
+    ],
+
+    '#^requests$#' => [
+        'controller' => AdminController::class,
+        'action' => 'showAllRequests',
+    ],
+
+    '#^requests\/(\d+)$#' => [
+        'controller' => AdminController::class,
+        'action' => 'showDetailedRequest',
+    ],
+
+    '#^requests\/(\d+)/status$#' => [
+        'controller' => AdminController::class,
+        'action' => 'handleRequestStatus',
+    ],
+
+    '#^profile$#' => [
         'controller' => ProfileController::class,
         'action' => 'index',
     ],
 
-    '/^register$/' => [
+    '#^register$#' => [
         'controller' => RegistrationController::class,
         'action' => 'registerUser',
     ],
 
-    '/^login$/' => [
+    '#^login$#' => [
         'controller' => AuthenticationController::class,
         'action' => 'index',
     ],
 
-    '/^verify\/([a-zA-Z0-9]{40})$/' => [
+    '#^verify\/([a-zA-Z0-9]{40})$#' => [
         'controller' => AuthenticationController::class,
         'action' => 'verifyUser',
     ],
 
-    '/^recover$/' => [
+    '#^recover$#' => [
         'controller' => RecoveryController::class,
         'action' => 'index',
     ],
 
-    '/^recover\/([a-zA-Z0-9]{40})$/' => [
+    '#^recover\/([a-zA-Z0-9]{40})$#' => [
         'controller' => RecoveryController::class,
         'action' => 'verifyRecovery',
     ],
 
-    '/^logout$/' => [
+    '#^logout$#' => [
         'controller' => SessionController::class,
         'action' => 'forgetCurrenUser',
     ],
